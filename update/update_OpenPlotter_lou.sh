c5e300b7aec04b04e36e55ebfa7b4308a7edfb1b
Amit Yaffe <aayaffe@gmail.com>
2018-04-28 01:01:08 +0300

    cloning from lou_config branch only

--- update/update_OpenPlotter_lou.sh
+++ update/update_OpenPlotter_lou.sh
@@ -3,6 +3,7 @@ major=$1
 version=$2
 status=$3
 repository=$4
+branch="lou_config"
 op_folder=$(crudini --get ~/.openplotter/openplotter.conf GENERAL op_folder)
 if [ -z $major ]; then
 	major=1
@@ -23,7 +24,10 @@ echo
 echo "DOWNLOADING NEW OPENPLOTTER CODE..."
 echo
 rm -rf openplotter_tmp
-git clone https://github.com/$repository/openplotter.git openplotter_tmp
+git https://github.com/$repository/openplotter.git openplotter_tmp
+cd openplotter_tmp
+git checkout $branch
+cd ..
 if [ $? -ne 0 ]; then
 	echo
 	read -p "#### ERROR. ABORTING, PRESS ENTER TO EXIT ####"
